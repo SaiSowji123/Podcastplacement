419d5448a102d7ecd6740551129a77ba9d396121
sainath161 <sainathsmart1234@gmail.com>
2024-02-23 20:10:40 +0530

    modify3

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,36 +1,40 @@
-import React, { useEffect, useState } from 'react';
+// Header.js
+
+import React, { useState } from 'react';
 import './styles.css';
 import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
   const location = useLocation();
   const currentPath = location.pathname;
+  const [menuVisible, setMenuVisible] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
+  const handleMenuToggle = () => {
+    setMenuVisible(!menuVisible);
+  };
 
-    window.addEventListener('scroll', handleScroll);
-    
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const handleLinkClick = () => {
+    setMenuVisible(false);
+  };
+
+  window.addEventListener('scroll', () => {
+    setScrolled(window.scrollY > 0);
+  });
 
   return (
     <div className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+      <div className='menu-bar' onClick={handleMenuToggle}>
+        <div className={`menu-icon ${menuVisible ? 'open' : ''}`}></div>
+        <div className={`menu-icon ${menuVisible ? 'open' : ''}`}></div>
+        <div className={`menu-icon ${menuVisible ? 'open' : ''}`}></div>
+      </div>
       <div className='gradient'></div>
-      <div className='links'>
-        <Link to="/" className={currentPath === "/" ? "active" : ""}>Signup</Link>
-        <Link to="/podcasts" className={currentPath === "/podcasts" ? "active" : ""}>Podcasts</Link>
-        <Link to="/create-a-podcast" className={currentPath === "/create-a-podcast" ? "active" : ""}>Start A Podcast</Link>
-        <Link to="/profile" className={currentPath === "/profile" ? "active" : ""}>Profile</Link>
+      <div className={`links ${menuVisible ? 'visible' : ''}`}>
+        <Link to="/" className={currentPath === "/" ? "active" : ""} onClick={handleLinkClick}>Signup</Link>
+        <Link to="/podcasts" className={currentPath === "/podcasts" ? "active" : ""} onClick={handleLinkClick}>Podcasts</Link>
+        <Link to="/create-a-podcast" className={currentPath === "/create-a-podcast" ? "active" : ""} onClick={handleLinkClick}>Start A Podcast</Link>
+        <Link to="/profile" className={currentPath === "/profile" ? "active" : ""} onClick={handleLinkClick}>Profile</Link>
       </div>
     </div>
   );
